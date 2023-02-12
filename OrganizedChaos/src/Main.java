b50cd36ba67ca840687b312ed1356aaef0b60dca
AndresEspositoDe <98119676+AndresEspositoDe@users.noreply.github.com>
2023-02-12 00:55:36 -0400

    :tada:Comienzo

---
+++ OrganizedChaos/src/Main.java
@@ -0,0 +1,17 @@
+
+import Interface.VentanaMenu;
+
+
+
+public class Main {
+
+    
+    public static void main(String[] args) {
+        
+        VentanaMenu v1 = new VentanaMenu();
+        v1.setVisible(true);
+        
+                
+    }
+    
+}
